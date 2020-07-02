103f60812c390dc8767187eb2316e34598e943ce
msjeon <jmlambda@naver.com>
2020-07-02 16:43:23 +0900

    Delete Example Comment

--- qjsonparser.cpp
+++ qjsonparser.cpp
@@ -21,33 +21,3 @@ QJsonParser::QJsonParser(QWidget *parent) : QWidget(parent){
 QJsonParser::~QJsonParser(){
 
 }
-//Example
-//QJsonObject obj;
-//obj.insert("one","Hi");
-//obj.insert("two","Bye");
-//obj.insert("b",false);
-//obj.insert("db",3.2);
-//obj.insert("int",3);
-//obj.insert("nul",QJsonValue::Null);
-//QJsonArray arr;
-//arr.append(1);
-//arr.append("STR");
-//QJsonArray arrarr;
-//arrarr.append(3);
-//arrarr.append(4);
-//arrarr.append(5);
-//arr.append(arrarr);
-//QJsonObject arrsub;
-//arrsub.insert("INARR","arr");
-//arr.append(arrsub);
-//obj.insert("MYARR",arr);
-//QJsonObject subObj;
-//subObj.insert("sub1","HH");
-//subObj.insert("sub2","HHH");
-//QJsonObject subsubObj;
-//subsubObj.insert("subsub",3);
-//subObj.insert("ss",subsubObj);
-//obj.insert("three",subObj);
-//obj.value("key").isObject();
-//qDebug()<<obj;
-//treeWidget->appendJson(obj);
